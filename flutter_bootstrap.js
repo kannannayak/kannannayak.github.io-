ef267cd815a5f0e6cd807f8721c32bc95e519adf
kannannayak <kannannyak200@gmail.com>
2024-12-06 18:52:16 +0530

    commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1710948204"
+    serviceWorkerVersion: "565602407"
   }
 });
